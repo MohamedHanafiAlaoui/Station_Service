58415cbc69454c2b02bed94acfc7bc9b5cf02654
MohamedHanafiAlaoui <hanafimohamed510@gmail.com>
2026-02-08 15:17:50 +0100

    git commit -m "Ajout du DTO JournalAuditDto avec validations pour typeAction, description et stationId

--- src/main/java/com/example/station_service/domain/journalAudit/dto/JournalAuditDto.java
+++ src/main/java/com/example/station_service/domain/journalAudit/dto/JournalAuditDto.java
@@ -1,5 +1,4 @@
-package com.example.station_service.dto;
-
+package com.example.station_service.domain.journalAudit.dto;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
