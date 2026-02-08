c1a63ea76600dd87aad1a12fb3002c5c45564fcd
MohamedHanafiAlaoui <hanafimohamed510@gmail.com>
2026-02-08 15:13:04 +0100

    Ajout de l'entité ApprovisionnementCarburant avec relation Station

--- src/main/java/com/example/station_service/domain/approvisionnementCarburant/entity/ApprovisionnementCarburant.java
+++ src/main/java/com/example/station_service/domain/approvisionnementCarburant/entity/ApprovisionnementCarburant.java
@@ -1,17 +1,16 @@
 package com.example.station_service.domain.approvisionnementCarburant.entity;
 
 import com.example.station_service.domain.approvisionnementCarburant.entity.enums.TypeCarburant;
+import com.example.station_service.domain.station.entity.Station;
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
-import java.util.Date;
+import java.time.LocalDate;
 
 @Entity
 @Table(name = "approvisionnementCarburant")
-@Data
+@Getter
+@Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
@@ -19,8 +18,10 @@ public class ApprovisionnementCarburant {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id ;
-    private Date dateApprovisionnement ;
+    private LocalDate dateApprovisionnement ;
     private Double quantite;
+    @Enumerated(EnumType.STRING)
     private TypeCarburant typeCarburant;
-
+    @ManyToOne @JoinColumn(name = "station_id")
+    private Station station;
 }
