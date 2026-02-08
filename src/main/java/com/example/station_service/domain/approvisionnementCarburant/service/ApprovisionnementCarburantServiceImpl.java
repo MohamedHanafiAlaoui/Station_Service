56edfdb327e84338ba686d982d229ad1bdf4f35d
MohamedHanafiAlaoui <hanafimohamed510@gmail.com>
2026-02-08 15:15:59 +0100

    Implémentation du service ApprovisionnementCarburantServiceImpl : création, récupération et rapports hebdo/mensuels

---
+++ src/main/java/com/example/station_service/domain/approvisionnementCarburant/service/ApprovisionnementCarburantServiceImpl.java
@@ -0,0 +1,74 @@
+package com.example.station_service.domain.approvisionnementCarburant.service;
+
+import com.example.station_service.domain.approvisionnementCarburant.dto.ApprovisionnementCarburantDto;
+import com.example.station_service.domain.approvisionnementCarburant.entity.ApprovisionnementCarburant;
+import com.example.station_service.domain.approvisionnementCarburant.mapper.ApprovisionnementCarburantMapper;
+import com.example.station_service.domain.approvisionnementCarburant.repository.ApprovisionnementCarburantRepository;
+import com.example.station_service.domain.station.entity.Station;
+import com.example.station_service.domain.station.repository.StationRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class ApprovisionnementCarburantServiceImpl implements ApprovisionnementCarburantService {
+
+    private final ApprovisionnementCarburantRepository repository;
+    private final StationRepository stationRepository;
+    private final ApprovisionnementCarburantMapper mapper;
+
+    @Override
+    public ApprovisionnementCarburantDto create(ApprovisionnementCarburantDto dto) {
+
+        ApprovisionnementCarburant entity = mapper.toEntity(dto);
+
+        Station station = stationRepository.findById(dto.getStationId())
+                .orElseThrow(() -> new RuntimeException("Station not found"));
+        entity.setStation(station);
+        entity.setDateApprovisionnement(LocalDate.now());
+        ApprovisionnementCarburant saved = repository.save(entity);
+        return mapper.toDto(saved);
+    }
+
+
+    @Override
+    public List<ApprovisionnementCarburantDto> getAll() {
+        return repository.findAll()
+                .stream()
+                .map(mapper::toDto)
+                .toList();
+    }
+
+    @Override
+    public ApprovisionnementCarburantDto getById(Long id) {
+        ApprovisionnementCarburant entity = repository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Approvisionnement not found"));
+
+        return mapper.toDto(entity);
+    }
+
+    @Override
+    public List<ApprovisionnementCarburantDto> getWeeklyReport() {
+        LocalDate end = LocalDate.now();
+        LocalDate start = end.minusWeeks(1);
+
+        return repository.findByDateApprovisionnementBetween(start, end)
+                .stream()
+                .map(mapper::toDto)
+                .toList();
+    }
+
+    @Override
+    public List<ApprovisionnementCarburantDto> getMonthlyReport() {
+        LocalDate end = LocalDate.now();
+        LocalDate start = end.minusMonths(1);
+
+        return repository.findByDateApprovisionnementBetween(start, end)
+                .stream()
+                .map(mapper::toDto)
+                .toList();
+    }
+}
