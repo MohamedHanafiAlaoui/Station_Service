313765e110380bc9db61740656eed0a0d690727c
MohamedHanafiAlaoui <hanafimohamed510@gmail.com>
2026-03-02 11:29:27 +0000

    Refactor BadgeRule interface and GoldBadgeRule implementation to remove VenteCarburant parameter from validate method, enhancing clarity and consistency in validation logic.

--- src/main/java/com/example/station_service/domain/client/rules/BadgeRule.java
+++ src/main/java/com/example/station_service/domain/client/rules/BadgeRule.java
@@ -1,10 +1,8 @@
 package com.example.station_service.domain.client.rules;
 
 import com.example.station_service.domain.client.entity.Client;
-import com.example.station_service.domain.venteCarburant.entity.VenteCarburant;
-
 import java.math.BigDecimal;
 
 public interface BadgeRule {
-    void validate(Client client, VenteCarburant vente, BigDecimal montant);
+    void validate(Client client, BigDecimal quantite, BigDecimal montant);
 }
--- src/main/java/com/example/station_service/domain/client/rules/GoldBadgeRule.java
+++ src/main/java/com/example/station_service/domain/client/rules/GoldBadgeRule.java
@@ -1,7 +1,6 @@
 package com.example.station_service.domain.client.rules;
 
 import com.example.station_service.domain.client.entity.Client;
-import com.example.station_service.domain.venteCarburant.entity.VenteCarburant;
 import org.springframework.stereotype.Component;
 
 import java.math.BigDecimal;
@@ -10,7 +9,7 @@ import java.math.BigDecimal;
 public class GoldBadgeRule implements BadgeRule {
 
     @Override
-    public void validate(Client client, VenteCarburant vente, BigDecimal montant) {
+    public void validate(Client client, BigDecimal quantite, BigDecimal montant) {
 
         if (client.getSolde().compareTo(montant) < 0) {
             throw new RuntimeException("Solde insuffisant pour un client GOLD");
