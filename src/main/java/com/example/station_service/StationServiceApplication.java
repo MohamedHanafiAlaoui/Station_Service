27e143cf694ed50c994d3a1c60335b02de156d2e
MohamedHanafiAlaoui <hanafimohamed510@gmail.com>
2026-03-28 08:47:37 +0100

    implement core domain services, security authentication, and badge-based fuel sales system

--- src/main/java/com/example/station_service/StationServiceApplication.java
+++ src/main/java/com/example/station_service/StationServiceApplication.java
@@ -1,18 +1,21 @@
 package com.example.station_service;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+
+@Slf4j
 @org.springframework.web.bind.annotation.RestController
 @SpringBootApplication
 @EnableCaching
 public class StationServiceApplication {
 	public static void main(String[] args) {
-        System.out.println("!!!!! STATION SERVICE STARTING - VERSION: ROBUST_V3_FINAL_CHECK !!!!!");
+        log.info("!!!!! STATION SERVICE STARTING - VERSION: ROBUST_V3_FINAL_CHECK !!!!!");
 		SpringApplication.run(StationServiceApplication.class, args);
 	}
     @org.springframework.web.bind.annotation.GetMapping("/api/test-robust")
     public String test() {
-        System.out.println(">>> RELIABILITY TEST REACHED - VERSION: V3");
+        log.info(">>> RELIABILITY TEST REACHED - VERSION: V3");
         return "ROBUST_V3_OK";
     }
-}
+}
